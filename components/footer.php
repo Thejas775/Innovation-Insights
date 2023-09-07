79f3293bd263eac9ed733a22b319c853e84a8135
Thejas Elandassery <58774753+Thejas775@users.noreply.github.com>
2023-09-07 09:29:53 +0530

    Initial commit

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="yourprojects.php"> <i class="fas fa-angle-right"></i> Your Projects</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Student Projects</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="tel:917028383929"><i class="fas fa-phone"></i> +91 7028383920</a>
+         <a href="tel:917780880809"><i class="fas fa-phone"></i> +91 7780880809</a>
+         <a href="mailto:1234@gmail.com"><i class="fas fa-envelope"></i> 1234@gmail.com</a>
+         <a href="https://www.google.com/maps/place/D.Y.+Patil+College+of+Engineering/@18.6448378,73.7391575,15z/data=!4m10!1m2!2m1!1sdypcoe!3m6!1s0x3bc2b9f1ca8dab03:0x6237cfbd36f9acf9!8m2!3d18.6448378!4d73.7582119!15sCgZkeXBjb2VaCCIGZHlwY29lkgEHY29sbGVnZZoBJENoZERTVWhOTUc5blMwVkpRMEZuU1VOS05GOWxVR2xuUlJBQuABAA!16s%2Fg%2F11_px5nll?entry=ttu"><i class="fas fa-map-marker-alt"></i> DYPCOE,Akurdi </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="www.facbook.com"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="www.twitter.com"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="www.instagram.com"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="www.linkedin.com"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Opensource Website <?= date('Y'); ?> </div>
+
+</footer>
